80006a06f5092b797a2cd2848f9e197713edfacf
MyManny <gmail.com>
2023-10-08 14:56:49 -0700

    components

--- personal-project/src/App.js
+++ personal-project/src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Contact from './components/Contact'
+import About from './components/About'
+import Projects from './components/Projects'
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="App">
-      
+      <About />
+      <Contact />
+      <Header />
+      <Projects />
     </div>
   );
 }
